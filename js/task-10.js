8801dec39ebdd785682719dfcff1f027546f0711
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-03-25 22:51:26 +0200

    add 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -5,4 +5,62 @@ function getRandomHexColor() {
 }
 
 
-//*******************************  В процесі виконання **************************
+//*******************************  неможу додуматись як збільшити кожен наступний юлок на 10px **************************
+
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+const controls = document.querySelector("#controls");
+
+ createBtn.addEventListener("click", getValue)
+function getValue(event) {
+ const currentInputvalue = event.currentTarget.previousElementSibling.value;
+
+    if(currentInputvalue !== "") {
+      createBoxes(Number(currentInputvalue));
+      event.currentTarget.previousElementSibling.value = "";
+    }
+
+  }
+
+ 
+  function createBoxes(amount) {
+
+    for( let i = 0; i < amount ; i += 1){
+      
+      if(i < amount){
+        const marcup = document.createElement("div");
+        marcup.style.backgroundColor = getRandomHexColor();
+        marcup.style.width = "30px";
+        marcup.style.height = "30px";
+
+       
+        boxesContainer.append(marcup)
+        
+      }
+      console.log(boxesContainer)
+    }}
+ 
+ 
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+destroyBtn.addEventListener("click", removeColection);
+function removeColection() {
+  boxesContainer.innerHTML = "";
+};
